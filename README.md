bf1ead4759251e2b175f71d2c7a27806a030239f
lovest3d <lovest3d@foxmail.com>
2013-09-12 09:54:55 +0800

    first add

---
+++ README.md
@@ -0,0 +1,29 @@
+android_bootable_recovery
+=========================
+
+CWM - based Recovery version 6.0.3.6 中文汉化版
+
+基于carliv的Recovey 汉化，
+
+并
+
+针对MTK做了一些处理和修改，
+
+   添加高级工具箱：
+       备份基带
+       恢复基带   //可备份和恢复基带（只针对MT6589）
+       关于       //显示一些操作信息或者作者信息
+
+添加时钟显示 电池电量显示
+添加按键震动
+
+加入一些操作提示。
+
+PS：可以去/recovey/Android.mk  下修改字体大小 默认为font_24x24.h
+    亦可以在device/xx/xx/BoardConfig.mk  用 BOARD_USE_CUSTOM_RECOVERY_FONT 定义字体大小
+
+作者：lovest3d   @ATX-Team 
+
+感谢：suky tenfar carliv
+
+
---
+++ default_recovery_ui.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/input.h>
+
+#include "recovery_ui.h"
+#include "common.h"
+#include "extendedcommands.h"
+
+char* MENU_HEADERS[] = { NULL };
+
+char* MENU_ITEMS[] = { "电源控制..",
+                       "安装zip刷机包",
+                       "清空所有数据",
+                       "清除缓存",
+                       "备份还原",
+                       "挂载选项",
+                       "高级设置",
+                       "工具箱",
+                       NULL };
+
+void device_ui_init(UIParameters* ui_parameters) {
+}
+
+int device_recovery_start() {
+    return 0;
+}
+
+int device_reboot_now(volatile char* key_pressed, int key_code) {
+    return 0;
+}
+
+int device_perform_action(int which) {
+    return which;
+}
+
+int device_wipe_data() {
+    return 0;
+}
